633c79764eb84eccc2dfa0af589b7117eeb62580
LuckyMan012359 <rorifloris@gmail.com>
2024-12-10 21:31:58 +0000

    feat: add sitemap.xsl code

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -49,6 +49,7 @@ const generateSitemap = (routes, domain) => {
     .join('\n');
 
   return `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`;
